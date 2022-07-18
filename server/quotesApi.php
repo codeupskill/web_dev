b70bb06acc534f71ad32fbd8fe2776f77595a20a
Muzaffar Ahmad <imuzaffarahmad@gmail.com>
2022-07-18 23:06:09 +0530

    php project added

---
+++ server/quotesApi.php
@@ -0,0 +1,79 @@
+<?php
+
+$data ="[{ 
+        'id' : 1,  
+        'title' : 'Title one',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/1/200/300'
+    },
+    { 
+        'id' : 2,  
+        'title' : 'Title two',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/2/200/300'
+    },
+    { 
+        'id' : 3,  
+        'title' : 'Title three',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/3/200/300'
+    },
+    { 
+        'id' : 4,  
+        'title' : 'Title four',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/4/200/300'
+    },
+    { 
+        'id' : 5,  
+        'title' : 'Title five',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/5/200/300'
+    },
+    { 
+        'id' : 6,  
+        'title' : 'Title six',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/6/200/300'
+    },
+    { 
+        'id' : 7,  
+        'title' : 'Title seven',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/7/200/300'
+    },
+    { 
+        'id' : 8,  
+        'title' : 'Title eight',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/8/200/300'
+    },
+    { 
+        'id' : 9,  
+        'title' : 'Title nine',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/9/200/300'
+    },
+    { 
+        'id' : 10,  
+        'title' : 'Title ten',
+        'quote' : 'The purpose of our lives is to be happy.',
+        'author' : 'Dalai Lama',
+        'image_url' : 'https://picsum.photos/id/10/200/300'
+    },
+]";
+
+echo $data;
+
+
+
+?>
